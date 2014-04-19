8b1621c1562fd50aba2566066a671bcb17f6a87c
Jan Linhart <admin@escope.cz>
2014-04-19 18:39:18 +0200

    contacts and documents

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,20 +4,29 @@ var umodrinuApp = angular.module('umodrinuApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ]);
 
 umodrinuApp.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/uvod', {
         templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/dokumenty', {
+        templateUrl: 'views/dokumenty.html',
         controller: 'MainCtrl'
       })
       .when('/mapa', {
         templateUrl: 'views/mapa.html',
         controller: 'MainCtrl'
       })
+      .when('/kontakt', {
+        templateUrl: 'views/kontakt.html',
+        controller: 'MainCtrl'
+      })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/uvod'
       });
   });
--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -1,5 +1,11 @@
 'use strict';
 
-umodrinuApp.controller('NavbarCtrl', ['$scope', '$route', function ($scope, $route) {
-    console.log($route.current.templateUrl);
-  }]);
+umodrinuApp.controller('NavbarCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.getActive = function(path) {
+	    if ($location.path().substr(0, path.length) == path) {
+	      return "active"
+	    } else {
+	      return ""
+	    }
+	}	
+}]);
